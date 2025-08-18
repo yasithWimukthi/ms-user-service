e2c8e1ce5ab0ef4353bde03372ade5e39c21938b
yasithwimukthi <wimukthiyasith@gmail.com>
2025-08-18 10:58:15 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/msuserservice/MsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.msuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/msuserservice/MsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.msuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
