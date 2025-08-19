4852e6b562e35bd0d4f8267b8094ae76091ee5c7
yasithwimukthi <wimukthiyasith@gmail.com>
2025-08-19 11:32:18 +0530

    config swagger

--- src/main/java/com/ecommerce/msuserservice/MsUserServiceApplication.java
+++ src/main/java/com/ecommerce/msuserservice/MsUserServiceApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.msuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class MsUserServiceApplication {
 
     public static void main(String[] args) {
